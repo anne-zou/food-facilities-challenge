97cb9121664157b9277253873778faa45cd33ee1
anzo <annezou97@gmail.com>
2026-01-28 16:36:39 -0800

    set up react + vite app with sqlite3db

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+  useEffect(() => {
+    // Load all mobile food facility data from local SQLite database
+    
+  } , [])
+
+  return (
+    <div className="App">
+      <h1>Food Facilities Challenge</h1>
+      <p>San Francisco Mobile Food Facility Permit Search</p>
+    </div>
+  )
+}
+
+export default App
